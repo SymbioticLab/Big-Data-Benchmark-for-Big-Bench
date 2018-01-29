8d78cfe17f96aeb7e7b40c77ca29d0605346fdbd
wentingt <wentingt@umich.edu>
2018-01-28 23:00:49 -0600

    q to query

--- runQuery.sh
+++ runQuery.sh
@@ -4,7 +4,8 @@ err=err_bb_1g
 for i in $(seq -f "%02g" 1 30)
 do 
 	echo $i
-	filename=/users/wentingt/Big-Data-Benchmark-for-Big-Bench/engines/hive/queries/q${i}/q${i}.sql
+	filename=/users/wentingt/Big-Data-Benchmark-for-Big-Bench/engines/hive/queries/q${i}/query${i}.sql
+	#filename=/users/wentingt/Big-Data-Benchmark-for-Big-Bench/engines/hive/queries/q${i}/q${i}.sql
 	echo "@@@$filename" >>$log
 	echo "@@@$filename" >>$err
 
